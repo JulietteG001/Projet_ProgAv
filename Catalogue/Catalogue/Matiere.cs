2cb23cc2aaf3d2278ef8d739d66fe155e65b9050
Xhantia <44496631+Xhantia@users.noreply.github.com>
2020-04-20 15:49:38 +0200

    Add files via upload

---
+++ Catalogue/Catalogue/Matiere.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue
+{
+    class Matiere
+    {
+        private string Nom { get; set; }
+        private string Coefficient { get; set; }
+        private List<Intervenant> Encadrants { get; set; } 
+
+        public Matiere() //Constructeur par défaut
+        {
+            Nom = "";
+            Coefficient = "0";
+            List<Intervenant> Encadrants = new List<Intervenant>();
+        }
+        public Matiere(string nom, string coefficient, List<Intervenant> encadrants) //Constructeur surchargé
+        {
+            Nom = nom;
+            Coefficient = coefficient;
+            List<Intervenant> Encadrants = encadrants;
+        }
+    }
+}
---
+++ Catalogue/Catalogue/Projet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue
+{
+    class Projet
+    {
+        private int NbEleves { get; set; }
+        private int Duree { get; set; } //Durée du projet en jours
+        private string Semestres { get; set; }
+        private string Consigne { get; set; }
+        private List<Livrable> Livrables { get; set; } 
+        private Matiere MatièreProjet { get; set; }
+        private List<Intervenant> Intervenants { get; set; } 
+
+        public Projet() //Constructeur par défaut
+        {
+            NbEleves = 0;
+            Duree = 0;
+            Semestres = "";
+            Consigne = "";
+            Livrables = new List<Livrable>();
+            Intervenants = new List<Intervenant>();
+        }
+        public Projet(int nbEleves, int duree, string semestres, string consigne, List<Livrable> livrables, List<Intervenant> intervenants) //Constructeur surchargé
+        {
+            NbEleves = nbEleves;
+            Duree = duree;
+            Semestres = semestres;
+            Consigne = consigne;
+            Livrables = livrables;
+            Intervenants = intervenants;
+        }
+    }
+}
