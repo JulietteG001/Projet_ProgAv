725104b7407ccc67649bed2066492979da717b8c
Lucie Thomasson <44496631+Xhantia@users.noreply.github.com>
2020-05-20 10:12:45 +0200

    prout

--- Catalogue/Catalogue/Program.cs
+++ Catalogue/Catalogue/Program.cs
@@ -13,37 +13,6 @@ namespace Catalogue
             //Lancement de la consultation du catalogue
             Consultation consultation = new Consultation();
             consultation.AfficherResultat(consultation.AffinerLaRecherche());
-            
-
-            Projet p = new Projet();
-            List<Projet> ps = new List<Projet>();
-
-            //ps = p.CritIntervenant("Kijewski");
-            //ps = p.CritMatiere("Introduction à la programmation");            
-            //ps = p.CritMatiere("Intelligence Artificielle");
-            //ps = p.CritAnnee("2019-2020");
-            //ps = p.CritLivrable("Etat de l'art");
-            //consultation.AfficherResultat(ps);
-
-            //ps = p.CritProjet("Projet Blackout");
-            //ps = p.CritLivrable("Rapport");
-
-            //foreach (Projet s in ps) //Pour vérifier ce que contient la liste renvoyée pendant les tests
-            //{
-            //    Console.WriteLine(s);
-            //}
-
-            //Console.WriteLine(p.CompteNoeuds("Projet"));
-
-            //AFFICHAGE DES INTERVENANTS
-
-            //foreach (Projet s in ps) //Pour vérifier ce que contient la liste renvoyée pendant les tests
-            //{
-            //    foreach (Intervenant i in s.Intervenants) //Pour regarder ce que contient la liste d'intervenants de chaque projet
-            //    {
-            //        Console.WriteLine("Nom : " + i.NomInterv.ToString());
-            //    }
-            //}
             Console.ReadLine();
         }
     }
